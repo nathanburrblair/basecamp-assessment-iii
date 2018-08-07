fc9a48c7d555a8c4b53722e91a20a967bb47d6e2
Nathan Blair <nathanburrblair@gmail.com>
2018-08-07 16:51:22 -0600

    Javascript 3 Stretch Problem

---
+++ app/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import HelloWorldList from './HelloWorldList';
+
+const App = () => {
+  return (
+    <div className = "App">
+      <HelloWorldList />
+    </div>
+  );
+}
+
+export default App;
